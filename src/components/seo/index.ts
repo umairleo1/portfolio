ac7f917c1978da616f6f646e238bf5ade7adcfa5
Muhammad Umair <umair.leo17@gmail.com>
2025-09-02 02:11:10 +0100

    feat: implement dynamic SEO meta tags with Open Graph and Twitter Cards

---
+++ src/components/seo/index.ts
@@ -0,0 +1,4 @@
+export { default as SEO } from './SEO';
+export { default as StructuredData } from './StructuredData';
+export { default as WebVitalsOptimizer } from './WebVitalsOptimizer';
+export * from './SEO';
