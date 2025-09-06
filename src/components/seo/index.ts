9d46877011d2f1a6b09e2779dd9ac77640a17917
Muhammad Umair <umair.leo17@gmail.com>
2025-09-06 16:07:04 +0100

    feat: enhance social media SEO with optimized meta tags and image assets

--- src/components/seo/index.ts
+++ src/components/seo/index.ts
@@ -1,4 +1,3 @@
 export { default as SEO } from './SEO';
 export { default as StructuredData } from './StructuredData';
 export { default as WebVitalsOptimizer } from './WebVitalsOptimizer';
-export * from './SEO';
